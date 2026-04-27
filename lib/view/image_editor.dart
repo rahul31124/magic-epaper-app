997b9122764c0ef03d78d6d79e14278ff0c39a19
rahul31124 <rdass87871@gmail.com>
2026-04-27 19:52:30 +0530

    fix ci

--- lib/view/image_editor.dart
+++ lib/view/image_editor.dart
@@ -20,8 +20,6 @@ import 'package:magicepaperapp/util/epd/epd.dart';
 import 'package:magicepaperapp/constants/color_constants.dart';
 import 'package:magicepaperapp/l10n/app_localizations.dart';
 import '../util/app_logger.dart';
-import 'package:magicepaperapp/provider/getitlocator.dart';
-
 
 class ImageEditor extends StatefulWidget {
   final DisplayDevice device;
@@ -578,7 +576,7 @@ class BottomActionMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-     final appLocalizations = AppLocalizations.of(context)!;
+    final appLocalizations = AppLocalizations.of(context)!;
     return SafeArea(
       top: false,
       bottom: true,
